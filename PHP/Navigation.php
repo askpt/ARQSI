04b2a8fd588a68da794a78adc8db81e17da3ee05
jpcarreira <jpmavcarreira@gmail.com>
2013-10-27 16:41:58 +0000

    added Navigation.php ref #9

---
+++ PHP/Navigation.php
@@ -0,0 +1,10 @@
+<?php 
+	// Navigation.php 
+?> 
+
+	<br><br> 
+
+	<a href=index.php>Home</a> | 
+	<a href=logout.php>Logout</a> 
+
+	<br><br> 
--- teste.php
+++ teste.php
@@ -8,8 +8,11 @@
 
 	include("PHP/Authentication.php");
 
+
 	echo "HOMEPAGE";
 	echo "<br><br>User: ".$_SESSION['susername'];
+
+	include("PHP/Navigation.php");
 
 
 ?>
