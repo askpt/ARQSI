bc0a0167b03666b8fd9b0e5ec61d8ff6baabc9b8
jpcarreira <jpmavcarreira@gmail.com>
2013-10-27 20:10:29 +0000

    added icons to navigation bar ref #10

--- PHP/Navigation.php
+++ PHP/Navigation.php
@@ -4,7 +4,17 @@
 
 	<br><br> 
 
-	<a href=index.php>Home</a> | 
-	<a href=PHP/Logout.php>Logout</a> 
+	<table align="center">
+		<tr>
+			<td><a href=index.php><img src="IMG/home.jpg" height="51" width="51"></a></td>
+			<td><a href=PHP/Logout.php><img src="IMG/logout.jpg" height="51" width="51"></a></td>
+		</tr>
+		<tr>
+			<td>Home</td>
+			<td>Logout</td>
+		</tr>
+
+	</table>
+	
 
 	<br><br> 
