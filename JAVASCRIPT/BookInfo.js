c40266dbe9df23d4c8a227c6aee5c574505e077e
askpt <andres_silva@outlook.com>
2013-10-31 16:16:50 +0000

    Added the Google Info to the dialog ref #6

--- JAVASCRIPT/BookInfo.js
+++ JAVASCRIPT/BookInfo.js
@@ -1,6 +1,7 @@
 var xmlHttpObj;
+var jsonHttpObjG;
 
-function stateHandlerListCategories()
+function stateHandlerBookInfo()
 { 
   if ( xmlHttpObj.readyState == 4 && xmlHttpObj.status == 200) 
   {
@@ -18,6 +19,7 @@ function stateHandlerListCategories()
    document.getElementById("dialog").innerHTML += "<p>" + isbn;
    document.getElementById("dialog").innerHTML += "<p>" + edition;
 
+   GetGoogleBookInfo(isbn);
  }
 }
 
@@ -30,7 +32,7 @@ function MakeXMLHTTPCallBookInfo(method, url)
 
     xmlHttpObj.open(method, url, true);
 
-    xmlHttpObj.onreadystatechange = stateHandlerListCategories;
+    xmlHttpObj.onreadystatechange = stateHandlerBookInfo;
     xmlHttpObj.send(null);
   }
 
@@ -42,4 +44,38 @@ function BookInfo (editor, book)
 {
   var url = "PHP/EditorAPI.php?type=GetBook&editor=" + editor + "&title=" + book;
   MakeXMLHTTPCallBookInfo("GET", url);
+}
+
+
+function GetGoogleBookInfo (isbn) {
+  var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+
+  jsonHttpObjG = CreateXmlHttpRequestObject();
+
+  if (jsonHttpObjG)
+  {
+
+    jsonHttpObjG.open("GET", url, true);
+
+    jsonHttpObjG.onreadystatechange = stateHandlerGoogleInfo;
+    jsonHttpObjG.send(null);
+  }
+
+}
+
+function stateHandlerGoogleInfo () {
+  if ( jsonHttpObjG.readyState == 4 && jsonHttpObjG.status == 200) 
+  {
+   var docJSON = jsonHttpObjG.responseText;
+
+   var json = eval('(' + docJSON + ')');
+
+   if (json.totalItems === 0) {
+    alert("desc:");
+   }else{
+    document.getElementById("dialog").innerHTML += "<p>" + json.items[0].volumeInfo.description;
+    document.getElementById("dialog").innerHTML += "<p><img src=\"" + json.items[0].volumeInfo.imageLinks.thumbnail + "\"></img>";
+   }
+
+ }
 }
