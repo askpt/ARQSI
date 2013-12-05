acf4372a7ce91317c9ae34d51956755a6ccff18f
André Silva <andres_silva@outlook.com>
2013-12-05 22:22:24 +0000

    Edited the Book class ref #14

---
+++ IDEIBiblio/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDEIBiblio.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
