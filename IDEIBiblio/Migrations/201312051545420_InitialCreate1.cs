15d2131e9c781089cfd6cb171fc7ddb18e2940d8
André Silva <andres_silva@outlook.com>
2013-12-05 15:46:38 +0000

    Configured the database migration tool.

---
+++ IDEIBiblio/Migrations/201312051545420_InitialCreate1.cs
@@ -0,0 +1,16 @@
+namespace IDEIBiblio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ IDEIBiblio/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace IDEIBiblio.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<IDEIBiblio.DAL.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "IDEIBiblio.DAL.ApplicationDbContext";
+        }
+
+        protected override void Seed(IDEIBiblio.DAL.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
