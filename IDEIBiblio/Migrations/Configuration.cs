c7b5c637578a36652dd3d8f952e3751111245115
jpcarreira <jpmavcarreira@gmail.com>
2013-12-06 12:25:56 +0000

    added Administrator and Manager users w/ corresponding roles ref #11

--- IDEIBiblio/Migrations/Configuration.cs
+++ IDEIBiblio/Migrations/Configuration.cs
@@ -21,28 +21,28 @@ namespace IDEIBiblio.Migrations
 
 
         // this method adds users and roles when Seed is called
-        bool AddUserAndRole(IDEIBiblio.DAL.ApplicationDbContext context)
+        bool AddUserAndRole(IDEIBiblio.DAL.ApplicationDbContext context, string userName, string pwd, string role)
         {
             IdentityResult identityResult;
 
             // creating a role
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            identityResult = roleManager.Create(new IdentityRole("CostumerRole"));
+            identityResult = roleManager.Create(new IdentityRole(role));
 
             // creating a user
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
             var user1 = new ApplicationUser()
             {
-                UserName = "Costumer1",
+                UserName = userName,
             };
-            identityResult = userManager.Create(user1, "Passw0rd1");
+            identityResult = userManager.Create(user1, pwd);
 
             // associating created user with role
             if (identityResult.Succeeded == false)
             {
                 return identityResult.Succeeded;
             }
-            identityResult = userManager.AddToRole(user1.Id, "CostumerRole");
+            identityResult = userManager.AddToRole(user1.Id, role);
 
             return identityResult.Succeeded;
         }
@@ -62,18 +62,19 @@ namespace IDEIBiblio.Migrations
             //    );
             //
 
-            // initializing DB with Editors
-
             // calling test user(s) and role(s)
-            AddUserAndRole(context);
+            AddUserAndRole(context, "Costumer1", "Passw0rd1", "CostumerRole");
+            AddUserAndRole(context, "Admin", "Adm1nistrator", "AdminRole");
+            AddUserAndRole(context, "Manager", "ProductMan4ger", "ManagerRole");
 
+            // initializing DB with Editors
             context.Editors.AddOrUpdate(
                     p => p.Name,
                     new Editor { Name = "Fruta Lda", Address = "Estadio das Antas", Email = "antas_a_arder@fruta.com", Phone = 222424671 },
                     new Editor { Name = "Bertrand", Address = "Rua das Flores 41", Email = "info@bertrand.pt", Phone = 21387430 }
             );
 
-
+            // initializing DB with Authors
             context.Authors.AddOrUpdate(
                 p => p.Name,
                 new Author { Name = "Pinto da Costa" },
@@ -82,6 +83,7 @@ namespace IDEIBiblio.Migrations
                 new Author { Name = "Hugo Miguel" }
             );
 
+            // initializing DB with Categories
             context.Categories.AddOrUpdate(
                 c => c.Name,
                  new Category { Name = "Terror" },
@@ -89,17 +91,21 @@ namespace IDEIBiblio.Migrations
                  new Category { Name = "Romance" }
              );
 
+            // initializing DB with Books
             context.Books.AddOrUpdate(
                 b => b.ISBN,
                 new Book { Title = "O Meu Apito Dourado", AuthorID = 3, EditorID = 1, Price = 15.99f, CategoryID = 1, ISBN = 1233122134, Year = 2012 },
                 new Book { Title = "Páginas Amarelas de Um Árbitro", AuthorID = 1, EditorID = 1, Price = 15.99f, CategoryID = 1, ISBN = 1253122134, Year = 2011 }
                 );
 
+            // initializing DB with Periodicals
             context.Periodicities.AddOrUpdate(
                 p => p.Name,
                 new Periodicity { Name = "Weekly", NumberofDays = 7 },
                 new Periodicity { Name = "Montly", NumberofDays = 30 }
                 );
+
+            // initializing DB with Magazines
             context.Magazines.AddOrUpdate(
                 m => m.Title,
                 new Magazine { Title="A Mística", AuthorID=2, CategoryID=2, EditorID=2, Issue="Ano 2013", Number="2", PeriodicityID=2, Price=3.99f, Publish= new DateTime(2013,2,19)},
