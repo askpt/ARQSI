4c5a129c647b501e6e0e741c2c8fef334bdd7acc
André Silva <andres_silva@outlook.com>
2013-12-07 14:49:45 +0000

    Created the base WS contracts for the website ref #18

---
+++ IDEIBiblioWS/IDEIBiblioWS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IDEIBiblioWS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class IDEIBiblioWS : IIDEIBiblioWS
+    {
+        public float GetBookPriceByID(int idBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetBookPriceByISBN(int ISBN)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetMagazinePriceByID(int idMagazine) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IDEIBiblioWS/IIDEIBiblioWS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IDEIBiblioWS
+{
+    [ServiceContract]
+    public interface IIDEIBiblioWS
+    {
+        [OperationContract]
+        float GetBookPriceByID(int idBook);
+
+        [OperationContract]
+        float GetBookPriceByISBN(int ISBN);
+
+        [OperationContract]
+        float GetMagazinePriceByID(int idMagazine);
+    }
+    
+}
