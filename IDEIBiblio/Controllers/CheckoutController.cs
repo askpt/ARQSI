fd8d6469c5e3d834959971cca9c3ca8ce991b772
jpcarreira <jpmavcarreira@gmail.com>
2013-12-08 20:32:40 +0000

    added Checkout controller prototype ref #15

---
+++ IDEIBiblio/Controllers/CheckoutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IDEIBiblio.Models;
+using IDEIBiblio.DAL;
+
+namespace IDEIBiblio.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        //
+        // GET: /Checkout/
+        public ActionResult ShowsAddressAndPayment()
+        {
+            return View();
+        }
+
+
+	}
+}
