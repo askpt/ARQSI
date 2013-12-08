5c24faf77a8c908035ef7a2529bde03b505d1be7
André Silva <andres_silva@outlook.com>
2013-12-08 17:04:20 +0000

    Added a reserved area for the product management ref #14, #11

---
+++ IDEIBiblio/Controllers/ManagerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDEIBiblio.Controllers
+{
+    public class ManagerController : Controller
+    {
+        //
+        // GET: /Manager/ManageProducts
+        public ActionResult ManageProducts()
+        {
+            return View();
+        }
+               
+    }
+}
