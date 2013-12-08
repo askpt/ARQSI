83f4f1f82854cc7dc4456e3470c3a3e12b2c5f76
jpcarreira <jpmavcarreira@gmail.com>
2013-12-08 14:42:28 +0000

    added ViewModels for displaying cart content and item-elimination confirmation ref #12

---
+++ IDEIBiblio/ViewModels/ShoppingCartRemoveViewModel.cs
@@ -0,0 +1,18 @@
+﻿// ViewModel to display a confirmation information when user wants to remove an item
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDEIBiblio.ViewModels
+{
+    public class ShoppingCartRemoveViewModel
+    {
+        public String Message { get; set; }
+        public decimal CartTotal { get; set; }
+        public int NumberOfItemsInCart { get; set; }
+        public int NumberOfSelectedItem { get; set; }
+        public int DeleteId { get; set; }
+    }
+}
---
+++ IDEIBiblio/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿// ViewModel to show the contents of the shopping cart
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IDEIBiblio.Models;
+
+namespace IDEIBiblio.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
