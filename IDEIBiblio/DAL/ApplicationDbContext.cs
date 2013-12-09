8ed45bd79dd9bfb1a561a07cb9777c709b710554
André Silva <andres_silva@outlook.com>
2013-12-09 11:35:20 +0000

    Updated Data Model

--- IDEIBiblio/DAL/ApplicationDbContext.cs
+++ IDEIBiblio/DAL/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ namespace IDEIBiblio.DAL
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("Gandalf")
         {
 
         }
